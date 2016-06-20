06a0aca802d75efec52d8e5c06189784d8c7878e
Javier Domingo Cansino <javier@jinnapp.com>
2016-06-20 12:51:36 +0100

    Making possible to execute it from the beginning

--- .config/ssh/sshd.sh
+++ .config/ssh/sshd.sh
@@ -6,4 +6,4 @@ if [[ ! -f $DIR/ssh_host_rsa_key ]]; then
 	ssh-keygen -f $DIR/ssh_host_rsa_key -N '' -t rsa
 fi
 
-$DIR/sshd -eD -f $DIR/sshd_config -h $DIR/ssh_host_rsa_key $* 
+$DIR/sshd -f $DIR/sshd_config -h $DIR/ssh_host_rsa_key $* 
